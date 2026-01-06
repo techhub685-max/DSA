b8a5ce7f7cb8b3b2d4903f9d5e983567af14a423
techhub685-max <techhub685@gmail.com>
2026-01-06 21:37:53 +0530

    Create LinkedListQueue.java

---
+++ Submissiob_III/LinkedListQueue.java
@@ -0,0 +1,49 @@
+class LinkedListQueue {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node front = null, rear = null;
+
+    void enqueue(int data) {
+        Node newNode = new Node(data);
+
+        if (rear == null) {
+            front = rear = newNode;
+            return;
+        }
+        rear.next = newNode;
+        rear = newNode;
+    }
+
+    void dequeue() {
+        if (front == null) {
+            System.out.println("Queue is empty");
+            return;
+        }
+        System.out.println("Dequeued: " + front.data);
+        front = front.next;
+    }
+
+    void display() {
+        Node temp = front;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue queue = new LinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        queue.display();
+    }
+}
