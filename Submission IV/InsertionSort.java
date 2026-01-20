24dff1f7886a80ce0889eccdf27d60ea5cb405b2
techhub685-max <techhub685@gmail.com>
2026-01-20 23:37:39 +0530

    Create InsertionSort.java

---
+++ Submission IV/InsertionSort.java
@@ -0,0 +1,21 @@
+class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
